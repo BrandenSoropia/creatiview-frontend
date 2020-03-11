844d1b58458436983f700096f20d944d6778b7f5
Branden Soropia <b.soropia@gmail.com>
2020-03-10 20:45:42 -0400

    Placeholder ThreadMarker component.

---
+++ src/Thread/ThreadMarker.svelte
@@ -0,0 +1,12 @@
+<script>
+  import FontAwesomeIcon from '../common/components/FontAwesomeIcon';
+  import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+</script>
+
+<style>
+
+</style>
+
+<!-- TODO: Add onclick -->
+<!-- TODO: Create container component for creating threads on click -->
+<FontAwesomeIcon tabindex="0" data={faMapMarkerAlt} />
---
+++ src/Thread/stories/ThreadMarker.stories.js
@@ -0,0 +1,8 @@
+import ThreadMarker from '../ThreadMarker.svelte';
+
+export default { title: 'Thread/ThreadMarker' };
+
+export const rendersThreadsMarker = () => ({
+  Component: ThreadMarker,
+  props: {}
+});
